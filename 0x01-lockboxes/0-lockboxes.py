8be6431d22f79ef406706a6cab9735ca05053350
idrissjuanito <idrissjuanito@gmail.com>
2024-03-14 23:14:38 +0100

    python interview exercise lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" breaking into lock boxes """
+
+
+def canUnlockAll(boxes):
+    """ functions unlock all boxes passed as param """
+    keys = set([0])
+    locked = list()
+    for i in range(len(boxes)):
+        if i in keys:
+            keys.update(boxes[i])
+        else:
+            locked.append(i)
+
+    if len(locked) > 0:
+        locked_copy = [*locked]
+        for i in locked_copy:
+            if i in keys:
+                locked.remove(i)
+                keys.update(boxes[i])
+    if len(locked) > 0:
+        return False
+    else:
+        return True
