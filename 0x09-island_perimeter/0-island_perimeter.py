8aad36f1f8a09402897369d57dae406a9f709b23
idrissjuanito <idrissjuanito@gmail.com>
2024-05-09 10:56:51 +0100

    island perimeter problem 2

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,14 +13,22 @@ def island_perimeter(grid):
         j = height_idx
         for j in range(1, len(grid[i]) - 1):
             if grid[i][j] == 1:
-                if grid[i - 1][j] != 1:
+                if grid[i + 1][j] == 0:
                     width += 1
-                    continue
-                elif grid[i - 1][j] == 1:
+                if grid[i][j - 1] == 0:
                     height += 1
-                    if (grid[i][j + 1] == 1):
-                        continue
-                    height_idx = j if height_idx == 0 else height_idx
-    height += 1
+
+
     perimeter = (width + height) * 2
     return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
