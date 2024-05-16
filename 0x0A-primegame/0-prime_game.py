2957bae770beb109ac5fbf66aad69416b26eb2dc
idrissjuanito <idrissjuanito@gmail.com>
2024-05-16 12:45:53 +0100

    third attempt at prime game: Fixed requirements

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """ Prime game module """
+primes = []
 
 
 def play(roundSet, playing, potentialWinner):
     """ executes a players move """
+
     if len(roundSet) == 1:
         return potentialWinner
     prime = roundSet[1]
+    primes.append(prime)
     for j in range(len(roundSet)):
         if j >= len(roundSet):
             break
-        if roundSet[j] % prime == 0:
+        num = roundSet[j]
+        if num % prime == 0:
             roundSet.pop(j)
     return play(roundSet, potentialWinner, playing)
 
@@ -20,15 +24,24 @@ def isWinner(x, nums):
     what if x is great than len nums
     what if len nums is greater that x
     """
-    players = {
-            "Maria": 0,
-            "Ben": 0
-            }
+    players = {"Maria": 0, "Ben": 0}
     round = 0
-    while round < x and round < len(nums):
-        roundSet = [i for i in range(1, nums[round] + 1)]
+    nums.sort()
+    prev_n = 0
+    while round < x:
+        if round >= len(nums):
+            n = nums[round - len(nums) - 1]
+        else:
+            n = nums[round]
+        roundSet = [1] if prev_n > 0 else []
+        roundSet += primes.copy()
+        for i in range(prev_n + 1, n + 1):
+            roundSet.append(i)
+        primes.clear()
         roundWinner = play(roundSet, "Maria", "Ben")
+        print(roundWinner)
         players[roundWinner] += 1
+        prev_n = n
         round += 1
     if players["Maria"] == players["Ben"]:
         return None
